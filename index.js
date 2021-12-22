9f44068b1abc01770e8175be025756fa6406d097
ilyosbek <ilyosbek.inomjonov.1996@gmail.com>
2021-12-22 22:49:27 +0500

    Done

---
+++ index.js
@@ -0,0 +1,16 @@
+// Agar string da "a" miz kichkinada bo'lsa false chiqarsin agar kattada bo'lsa true chiqarsin
+
+// Variation 1
+
+// function app(params) {
+//     if(params === params.toUpperCase()) console.log(true);
+//     else console.log(false);
+// }
+// app('A')
+
+// Variation 2
+
+// function app(params) {
+//     console.log(params === params.toUpperCase());
+// }
+// app('A')
